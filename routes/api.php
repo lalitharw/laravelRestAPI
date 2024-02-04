5d6d29f9f5b7cc4c78faba76e15e2151061bb478
lalitharw <lalitharwate90@gmail.com>
2024-02-04 14:07:18 +0530

    created api token and also added group route

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\StudentController;  
+use App\Http\Controllers\StudentController;
+use App\Http\Controllers\UserController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -12,14 +13,18 @@ use App\Http\Controllers\StudentController;
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "api" middleware group. Make something great!
 |
-*/ 
+*/
 
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
-Route::get("/student",[StudentController::class,"index"]);
-Route::get("/student/{id}",[StudentController::class,"show"]);
-Route::post("/student",[StudentController::class,"store"]);
-Route::put("/student/{id}",[StudentController::class,"update"]);
-Route::delete("/student/delete/{id}",[StudentController::class,"destroy"]);
-
+Route::post("/register",[UserController::class,"register"]);
+Route::middleware('auth:sanctum')->group([
+    function () {
+        Route::get("/student", [StudentController::class, "index"]);
+        Route::get("/student/{id}", [StudentController::class, "show"]);
+        Route::post("/student", [StudentController::class, "store"]);
+        Route::put("/student/{id}", [StudentController::class, "update"]);
+        Route::delete("/student/delete/{id}", [StudentController::class, "destroy"]);
+    }
+]);
