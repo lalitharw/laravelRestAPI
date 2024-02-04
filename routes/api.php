67ab6c8d72dde2945e52a7641d732629510d4ae8
lalitharw <lalitharwate90@gmail.com>
2024-02-04 13:02:04 +0530

    .

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\StudentController;
+use App\Http\Controllers\StudentController;  
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -12,7 +12,7 @@ use App\Http\Controllers\StudentController;
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "api" middleware group. Make something great!
 |
-*/
+*/ 
 
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
